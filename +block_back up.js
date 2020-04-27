a62ad768ef4d9f5256dadd5604dcd16ae2f3cfd2
kang87y <55308474+kang87y@users.noreply.github.com>
2020-04-27 11:50:08 +0900

    Update +block_back up.js

--- +block_back up.js
+++ +block_back up.js
@@ -340,7 +340,7 @@ const addBlock = (blockname, template, color, params, _class, func, skeleton = '
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-addBlock('stop_project', '작품 정지시키기     ', {
+addBlock('stop_project_with_error', '작품 정지시키기     ', {
 }, {
     params: [
         {
@@ -353,7 +353,7 @@ addBlock('stop_project', '작품 정지시키기     ', {
         }
     ],
 }, 'text', (sprite, script) => {
-     openProject();
+     Entry.engine.toggleStop();
 })
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -372,7 +372,7 @@ addBlock('day', '요일', {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-addBlock('mouse_which_clicked', '마우스 좌/우/휠클릭     ', {
+addBlock('which_mouse_clicked', '마우스 좌/우/휠클릭   ', {
 }, {
     params: [],
     def: [],
@@ -405,53 +405,101 @@ logMouseButton;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-addBlock('boost_mode', '부스트모드%1     ', {
+addBlock('boost_mode', '부스트모드가 켜져있는가?   ', {
 }, {
     params: [
-    {
-                type: "Dropdown",
-                options: [
-                    [ "ON", "0" ],
-                    [ "OFF", "1" ]
-                ],
-            }
     ],
     def: [],
-    paramsKeyMap: {
-            VALUE: 0
-    },
     map: {}
 }, 'text', (sprite, script) => {
-        const value = script.getNumberField("VALUE", script);
-            let result;
-            switch(value) {
-                case 0:
-                    if (useWebGL == true) {
-                        result = true;
-                    } else {
-                        result = false;
-                    }
-                    break;
-                case 1:
-                    if (useWebGL == 'true') {
-                        result = false;
-                    } else {
-                        result = true;
-                    }
-                    break;
-            }
-
-            return result;
+    if (useWebGL == true) {
+        return true;
+    } else {
+        return false;
+    }
 }, 'basic_boolean_field')
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+addBlock('alert', '%1제목의 alert(경고창) 띄우기     ', {
+}, {
+    params: [
+        {
+            type: 'Block',
+            accept: 'string'
+        },
+        {
+            type: 'Indicator',
+            size: 11,
+        }
+    ],
+    def: [
+        {
+            type: "text",
+            params: [`엔트리`]
+        },
+    ],
+    map: {
+        VALUE: 0
+    },
+}, 'text', (sprite, script) => {
+    const value = script.getValue('VALUE', script);
+    alert(value);
+})
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+addBlock('box', '%1제목의 %2 띄우기   ', {
+}, {
+    params: [
+        {
+            type: "Block",
+            accept: "string"
+        },
+        {
+            type: "Dropdown",
+            options: [
+                ['confirm(선택창)', '1'],
+                ['prompt(입력창)', '2']
+            ],
+         fontSize: 11,
+         value: 'confirm(선택창)'
+        },
+    ],
+    def: [
+                {
+                    type: 'text',
+                    params: [`엔트리`]
+                },
+                null
+    ],
+    map: {
+         LEFTHAND: 0,
+         RIGHTHAND: 1
+    },
+}, 'text', (sprite, script) => {
+    const leftValue = script.getValue('LEFTHAND', script);
+    const rightValue = script.getField('RIGHTHAND', script);
+    
+    if (rightValue == '1') {
+        let choose = confirm(leftValue);
+        return choose;
+    } else if (rightValue == '2') {
+        let enter = prompt(leftValue);
+        return enter;
+    }
+}, 'basic_string_field')
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 Entry.staticBlocks.push({
     category: 'API', blocks: [
-        'stop_project',
+        'stop_project_with_error',
+        'boost_mode',
         'day',
-        'boost_mode'
+        'alert',
+        'box'
     ]
 });
 
