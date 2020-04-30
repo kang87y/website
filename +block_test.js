c67ff4c40423fe03bcc9f0815ce94537d70ab6e4
kang87y <55308474+kang87y@users.noreply.github.com>
2020-04-30 11:21:36 +0900

    Update +block_test.js

--- +block_test.js
+++ +block_test.js
@@ -321,8 +321,8 @@ const updateCategory = (category, options) => {
 }
 const addBlock = (blockname, template, color, params, _class, func, skeleton = 'basic') => {
     Entry.block[blockname] = {
-        color: '#4c0099',
-        outerLine: '#330066',
+        color: '#C0C0C0',
+        outerLine: '#808080',
         skeleton: skeleton,
         statement: [],
         params: params.params,
